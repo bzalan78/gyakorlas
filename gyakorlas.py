32b707eeb9b80051a6cf9b255fe049b45eaed77a
bzalan78 <bokanyizalan@turr.hu>
2023-12-13 10:23:03 +0100

    választólehetőség hozzáadása

--- gyakorlas.py
+++ gyakorlas.py
@@ -1,3 +1,4 @@
+k=int(input("Ha egy szám négyzetét (1), vagy ha 2-szereséd szeretnéd(2). "))
 n=int(input("adj meg egy számot aminek a négyzetét kéred "))
 print(n*n)
 l=int(input("adj meg egy számot aminek a kétszeresét írom ki "))
