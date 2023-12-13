9f335e4a9abe7c27ac220ba63a9ea0183fdf6196
bzalan78 <bokanyizalan@turr.hu>
2023-12-13 10:31:52 +0100

    Utolsó +if ha nem kell semmitcsináljon

--- gyakorlas.py
+++ gyakorlas.py
@@ -1,9 +1,12 @@
-k=int(input("Ha egy szám négyzetét (1), vagy ha 2-szereséd szeretnéd(2). "))
+k=int(input("Ha egy szám négyzetét (1), vagy ha 2-szereséd szeretnéd(2) de ha semmi nem kell akkor (3). "))
 s=1
 d=2
+h=3
 if k==s:
     n=int(input("adj meg egy számot aminek a négyzetét kéred "))
     print(n*n)
 elif k==d:
     l=int(input("adj meg egy számot aminek a kétszeresét írom ki "))
     print(l*2)
+elif k==h:
+    
